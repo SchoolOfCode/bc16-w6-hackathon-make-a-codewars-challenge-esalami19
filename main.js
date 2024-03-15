88498424eb43fe9f2e716b3d10271b84a3082086
User <esalami19@yahoo.co.uk>
2024-03-15 12:55:34 +0000

    v1, test pass

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*match match drug A with people who have said 'yes' to allergies and drug b to people 
+/*match drug A with people who have said 'yes' to allergies and drug b to people 
 who have said no to allergies and return the drug assigned to them*/
 
 function clinicalTrial(patient) {}
--- main.test.js
+++ main.test.js
@@ -1,36 +1,33 @@
 import { expect, test } from "vitest";
 
-import clinicalTrial from "./main.js";
+import clinicalTrial from "./kata.js";
 
-// test("no function to pass test", () => {
-// 	//arrange
-// 	const patient1 = "no allegies";
-// 	const patient2 = "allergies";
-// 	const expected = "a" || "b";
-// 	//act
-// 	const actual = clinicalTrial(
-// 		testSubjects.patient1.drug,
-// 		testSubjects.patient2.drug,
-// 	);
-// 	//assert
-// 	expect(actual).toBe(expected);
-// });
-test("modified test for version 1 function", () => {
+test("Test for patients with allergies", () => {
 	//arrange
-	patient.allergy = "yes";
+	const patient = {
+		name: "Denise Camel",
+		age: 29,
+		medicalDiagnosis: "healthy",
+		allergies: "yes",
+	};
 	const expected = "a";
 	//act
 	const actual = clinicalTrial(patient);
 	//assert
 	expect(actual).toBe(expected);
 });
-// test("test", () => {
-// 	//arrange
-// 	const a = 1;
-// 	const b = 2;
-// 	const expected = 3;
-// 	//act
-// 	const actual = sum(a, b);
-// 	//assert
-// 	expect(actual).toBe(expected);
-// });
+
+test("Test for patients without allergies", () => {
+	//arrange
+	const patient = {
+		name: "Denise Camel",
+		age: 29,
+		medicalDiagnosis: "healthy",
+		allergies: "no",
+	};
+	const expected = "b";
+	//act
+	const actual = clinicalTrial(patient);
+	//assert
+	expect(actual).toBe(expected);
+});
